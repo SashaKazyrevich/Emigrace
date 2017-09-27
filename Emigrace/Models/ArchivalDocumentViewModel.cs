71ad66357a0e5ac9226b7be4fee0aadc7adf81ba
Sasha Kazyrevich <a.kazyrevich@gmail.com>
2017-09-27 12:11:18 +0200

    Update single pge for an archive

---
+++ Emigrace/Models/ArchivalDocumentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Emigrace.Models
+{
+    public class ArchivalDocumentViewModel
+    {
+        [Required]
+        public int Id { get; private set; }
+
+        [Required]
+        public string FondName { get; private set; }
+
+        [Required]
+        public string TimeInterval { get; private set; }
+
+        public string Language { get; private set; }
+
+    }
+}
