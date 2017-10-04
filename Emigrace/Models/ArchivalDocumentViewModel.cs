699fdbc3b724a1597bc8bfd2a2832e4780ab66a8
Sasha Kazyrevich <a.kazyrevich@gmail.com>
2017-10-04 08:42:33 +0200

    Change schema for ArchivalDocuments table, add tables ArchivalDocumentPages and BooksPages

--- Emigrace/Models/ArchivalDocumentViewModel.cs
+++ Emigrace/Models/ArchivalDocumentViewModel.cs
@@ -6,15 +6,17 @@ namespace Emigrace.Models
     public class ArchivalDocumentViewModel
     {
         [Required]
-        public int Id { get; private set; }
+        public int Id { get; set; }
 
         [Required]
-        public string FondName { get; private set; }
+        public string FondName { get; set; }
+
+        public string FondNumber { get; set; }
 
         [Required]
-        public string TimeInterval { get; private set; }
+        public string TimeInterval { get; set; }
 
-        public string Language { get; private set; }
+        public string Language { get; set; }
 
     }
 }
--- Scripts/Database/v0.1/0.3.schema.sql
+++ Scripts/Database/v0.1/0.3.schema.sql
@@ -16,23 +16,53 @@ BEGIN
 END
 GO
 
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'ArchivalFonds')
+BEGIN
+	PRINT 'Create ArchivalFonds table'
+	CREATE TABLE [dbo].[ArchivalFonds] (
+
+		Id BIGINT CONSTRAINT PK_ArchivalFonds_Id PRIMARY KEY IDENTITY(1,1) NOT NULL,
+		FondNumber NVARCHAR(50) NOT NULL,
+		FondName NVARCHAR(100) NULL,
+                ArchiveId BIGINT NOT NULL,
+	        CONSTRAINT FK_ArchivalFonds_ArchiveId FOREIGN KEY(ArchiveId) REFERENCES Archives(Id)
+		)
+END
+GO
+
+
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'ArchivalDocuments')
 BEGIN
 	PRINT 'Create ArchivalDocuments table'
 	CREATE TABLE [dbo].[ArchivalDocuments] (
-		Id BIGINT CONSTRAINT PK_ArchivalDocuments_Id PRIMARY KEY IDENTITY(1,1) NOT NULL,
-		ArchiveId BIGINT NOT NULL,
-	        CONSTRAINT FK_ArchivalDocuments_ArchiveId FOREIGN KEY(ArchiveId) REFERENCES Archives(Id),
-		FondNumber NVARCHAR(50) NOT NULL,
-		FondName NVARCHAR(100) NULL,
+		Id BIGINT CONSTRAINT PK_ArchivalDocuments_Id PRIMARY KEY IDENTITY(1,1) NOT NULL,         
+		FondId BIGINT NOT NULL,
+	        CONSTRAINT FK_ArchivalDocuments_FondId FOREIGN KEY(FondId) REFERENCES ArchivalFonds(Id),
 		InventoryNumber int NULL,
-		DocumentNumber int NOT NULL,
+		FileNumber int NULL,
+		DocumentNumber int NOT NULL,                            
 		TimeInterval VARCHAR(50),
 		Language CHAR(10) NULL 
 		
 	)
 END
 GO
+
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'ArchivalDocumentPages')
+BEGIN
+	PRINT 'Create ArchivalDocumentPages table'
+	CREATE TABLE [dbo].[ArchivalDocumentPages] (
+        Id BIGINT CONSTRAINT PK_ArchivalDocumentPages_Id PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	DocumentId BIGINT NOT NULL,
+	CONSTRAINT FK_ArchivalDocumentPages_DocumentId FOREIGN KEY(DocumentId) REFERENCES ArchivalDocuments(Id),
+	PhotoNumber string NULL
+	)
+END
+GO        
+
+
+
 
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Books')
 BEGIN
@@ -45,6 +75,18 @@ BEGIN
 		TimeInterval VARCHAR(50),
 		Language CHAR(10)
 		
+	)
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'BookPages')
+BEGIN
+	PRINT 'Create BookPages table'
+	CREATE TABLE [dbo].[BookPages] (
+        Id BIGINT CONSTRAINT PK_BookPages_Id PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	BookId BIGINT NOT NULL,
+	CONSTRAINT FK_BookPages_BookId FOREIGN KEY(BookId) REFERENCES Books(Id),
+	PhotoNumber string NULL
 	)
 END
 GO
